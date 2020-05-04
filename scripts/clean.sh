0c26f753aff2064dbd4b44e46b85a888628c2939
qrzeller <quentin@sportiworld.com>
2020-05-04 15:41:59 +0000

    script to clean ROS

---
+++ scripts/clean.sh
@@ -0,0 +1,2 @@
+wget https://gist.githubusercontent.com/superseb/2cf186726807a012af59a027cb41270d/raw/eaa2d235e7693c2d1c5a2a916349410274bb95a9/cleanup.sh && chmod a+x ./cleanup.sh && sudo ./cleanup.sh
+
---
+++ scripts/git-clone.sh
@@ -0,0 +1,1 @@
+alias git="docker run -ti --rm -v $(pwd):/git bwits/docker-git-alpine"
